6d6e37e2bd553166577e0965153feb620ffde6de
Francisco López <54652030+pacool84@users.noreply.github.com>
2020-04-22 08:23:09 -0500

    Add Resume and AboutMe components

---
+++ src/components/AboutMe.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AboutMe extends Component{
+    render(){
+        return(
+            <h1>Hello AboutMe</h1>
+        )
+    }
+};
+
+export default AboutMe;
