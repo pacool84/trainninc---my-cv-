338386ad1fc3d970cfc052b4411a3bb801a557fa
Francisco López <54652030+pacool84@users.noreply.github.com>
2020-04-22 08:15:46 -0500

    Add mdl-react for the navbar layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello My Friend</h1>
+    <div className="demo-big-content">
+    <Layout>
+        <Header title="Title" scroll>
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+        </Content>
+    </Layout>
+</div>
   );
 }
 
