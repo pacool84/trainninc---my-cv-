bd06fd3fefe7062acaad72ef0ff9392c2a250782
Francisco López <54652030+pacool84@users.noreply.github.com>
2020-04-23 10:24:50 -0500

    Add more content to the RESUME component and also more Styles

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 import idProfile from '../assets/statics/IdProfile.png'
+import iconSn from '../assets/statics/IconSN.png'
 
 class Resume extends Component{
     render(){
@@ -12,12 +13,20 @@ class Resume extends Component{
                       src={idProfile}
                       atl="avatar"/>
                       <p className="resume-content-profile">
-                          <h1>Professional Experience</h1>
-                          I have over 10 years of experience in ServiceDesk management, support areas, supplier management and incident and request process management.<br></br> <br></br>
-                        Precursor of the ServiceNow project, being an essential part in the adoption of the tool in the region of Mexico and CAM, achieving standardization of processes worldwide and improving the life cycle in the care of incidents, requests, changes, events and problems.<br></br> <br></br>
-                        Additionally, I have experience in the design and development of key indicators, dashboards and solutions for other areas and countries (Canada, CAM, Chile, E- Commerce MX) with the objective of maturing processes and improving services related to IT 
+                          <h1>Professional Resume</h1>
+                          I have over 10 years of experience in <strong>ServiceDesk management</strong> , support areas, supplier management and <strong>incident and request process management.</strong><br></br> <br></br>
+                        Precursor of the <strong>ServiceNow</strong> project, being an essential part in the adoption of the tool in the region of Mexico and CAM, <strong>achieving standardization of processes</strong> worldwide and improving the life cycle in the care of incidents, requests, changes, events and problems.<br></br> <br></br>
+                        Additionally, I have experience in the <strong>design and development of key indicators</strong>, dashboards and solutions for other areas and countries (Canada, CAM, Chile, E- Commerce MX) with the objective of maturing processes and improving services related to IT 
+                        
                         </p>
-                        <img src=""></img>
+
+                        <section className="resume-content-experience">
+                            <h3>Profesional Experience</h3>
+                            <p>Service Desk Co. Manager
+Incident and Request processes manager for the region of Mexico, regional administrator of the Servicenow tool as well as responsible for the operational areas, Servicedesk, First level support for Windows and UNIX Servers, Telecommunications Infrastructure and Event Monitoring in Sites of systems.
+My main objectives and activities were to develop and implement technological solutions through the Servicenow tool that allowed to automate and improve activities within the processes of incidents, requests and events, these solutions were focused both for internal users and for the areas of IT support .</p>          
+                        </section>
+                        
 
                     </Cell>        
                 </Grid>
