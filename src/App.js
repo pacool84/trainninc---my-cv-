b40c9fca580e07f3eca0996ca79c57f54347ed2e
Pacool84 <flopez_1384@hotmail.com>
2023-01-08 13:06:19 -0600

    [ADD] Last changes to upload in to the cloud

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
-import React from 'react';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import { Link } from 'react-router-dom';
-import './App.css';
-import Main from './components/Main';
+import React from "react";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import { Link } from "react-router-dom";
+import "./App.css";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="demo-big-content" >
-    <Layout>
-        <Header style={{display: 'flex'}} className="header-nav" title="Francsico Lopez Campos" scroll>
-            <Navigation>
-                <Link to="/">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/contact">Contact</Link>
-                <a to="/">Link</a>
-            </Navigation>
-
-            
+    <div className="demo-big-content">
+      <Layout>
+        <Header
+          style={{ display: "flex" }}
+          className="header-nav"
+          title="Francsico Lopez Campos"
+          scroll
+        >
+          <Navigation>
+            <Link to="/">Resume by PACOOL </Link>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/contact">Contact</Link>
+            <a to="/">Link</a>
+          </Navigation>
         </Header>
 
         <Drawer title="@babasyop">
-            <Navigation style={{color: 'white'}}>
-                <Link to="/">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="contact">Contact</Link>
-                <a to="/">Link</a>
-            </Navigation>
+          <Navigation style={{ color: "white" }}>
+            <Link to="/">Resume</Link>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="contact">Contact</Link>
+            <a to="/">Link</a>
+          </Navigation>
         </Drawer>
         <Content>
-            <div className="page-content" />
-            <Main />
+          <div className="page-content" />
+          <Main />
         </Content>
-    </Layout>
-</div>
+      </Layout>
+    </div>
   );
 }
 
